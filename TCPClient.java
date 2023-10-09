64f70c8c9c423908a81ba2bd7f0b69a19b380023
JJ Barrera <jfbarrera2002@gmail.com>
2023-10-09 17:36:38 -0500

    Just starting out.

--- TCPClient.java
+++ TCPClient.java
@@ -1,9 +1,53 @@
+/***********************************************************************************************************
+ ** Jose Barrera																						  **
+ ** COSC 4360																							  **
+ ** 10-4-2023																						      **
+ ** Purpose: Make the client take in a file from the command line, send it to the server, hash the file   **
+ ** using the SHA-256 algorith, compare the recieved server hash to its own, show the RTT, and            **
+ ** calculate the throughput (in bits).																      **
+ ***********************************************************************************************************/
+
 import java.net.*;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.io.*;
+import java.math.BigInteger;
+
 import javax.swing.JOptionPane;
 
 public class TCPClient 
 {
+	/***
+	 * Method that encrypts an input String using the SHA-256 algorthim.
+	 * @param input The String to encrypt.
+	 * @return The encrypted String.
+	 * @throws NoSuchAlgorithmException
+	 */
+	public String encryptString(String input) throws NoSuchAlgorithmException
+ 	{
+        //MesageDigest works with MD2, MD5, SHA-1, SHA-224, SHA-256
+        //SHA-384, and SHA-512
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+
+        byte[] messageDigest = md.digest(input.getBytes());
+
+        BigInteger bigInt = new BigInteger(1, messageDigest);
+        
+        return bigInt.toString(16);
+    }
+
+	/**
+	 * Method that computes the throughput. 
+	 * @param fileSizeInBits The size of the file in bits.
+	 * @param timeTaken The time taken to recieve the file.
+	 * @return The throughput.
+	 */
+	public double throughput(int fileSizeInBits, double timeTaken){
+
+		double tp = (double) fileSizeInBits / timeTaken;
+
+		return tp;
+	}
 
 	public static void main(String[] args) 
 	{
--- TCPServer.java
+++ TCPServer.java
@@ -1,8 +1,35 @@
+/*Jose Barrera
+ * COSC 4360
+ * 10-4-2023
+ * Purpose: Right Now, I don't know.
+ */
+
 import java.net.*;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.io.*;
+import java.math.BigInteger;
 
 public class TCPServer 
 {
+	/**
+	 * Method that encrypts a String using the SHA-256 algorthim.
+	 * @param input The String to encrypt.
+	 * @return An encrypted String.
+	 * @throws NoSuchAlgorithmException 
+	 */
+	public String encryptString(String input) throws NoSuchAlgorithmException
+ 	{
+        //MesageDigest works with MD2, MD5, SHA-1, SHA-224, SHA-256
+        //SHA-384, and SHA-512
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+
+        byte[] messageDigest = md.digest(input.getBytes());
+
+        BigInteger bigInt = new BigInteger(1, messageDigest);
+        
+        return bigInt.toString(16);
+    }
 	
 	public static void main(String[] args) 
 	{
